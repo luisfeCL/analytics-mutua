2becbf66f7c2f354c2d552ca046c4bd09b064375
luisfeCL <92877836+luisfeCL@users.noreply.github.com>
2024-06-10 15:53:41 +0200

    select custom event

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.boton');
-    let counter = 0;
 
-    button.addEventListener('click', () => {
-        counter++
-        console.log(`click: ${ counter }`)
-    })
-})
+    let counter = 0;
+    
+    const combo = document.querySelector('select');
+    button = document.querySelector('.boton'),
+    ev = new Event('optionSelected');
+
+    // Escuchar el evento 'change' y despachar el evento personalizado 'optionSelected'
+    combo.addEventListener('change', () => { 
+        document.dispatchEvent(ev);
+    });
+
+    button.addEventListener('click', () => { 
+        counter++;
+        console.log(`click: ${ counter }`)})
+
+    // Manejador para el evento personalizado 'optionSelected'
+    document.addEventListener('optionSelected', () => {
+        const selectedIndex = combo.selectedIndex;
+        console.log(`Elemento ${ selectedIndex + 1 } seleccionado`);
+    });
+
+});
